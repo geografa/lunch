667c7d29f7066ec70ed6b58234b080fe7df5784f
Rafa Gutierrez <rafa@mapbox.com>
2020-03-15 00:27:16 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase
+var config = {
+    apiKey: "AIzaSyBo3SOZBeV-k2K_vf8_Qi01WZ7FSIznY1s",
+    authDomain: "pickup-going.firebaseapp.com",
+    databaseURL: "https://pickup-going.firebaseio.com",
+    projectId: "pickup-going",
+    storageBucket: "pickup-going.appspot.com",
+    messagingSenderId: "343546099483",
+    appId: "1:343546099483:web:1b58f74c1c7b90c00185be"
+  };
